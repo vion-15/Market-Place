c7888554ef609d2fd04899d04e407569615fb76c
Ahmad <ahmadfaidh2003@gmail.com>
2026-04-12 20:47:30 +0700

    build struct for users

---
+++ backend/models/users.model.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+)
+
+type Users struct {
+	Id              int64      `json:"id" db:"id"`
+	Name            string     `json:"name" db:"name"`
+	Email           string     `json:"email" db:"email"`
+	Phone           string     `json:"phone" db:"phone"`
+	Password        string     `json:"-" db:"password"`
+	PhotoProfile    string     `json:"link" db:"link"`
+	IsEmailVerified bool       `json:"is_email_verified" db:"is_email_verified"`
+	IsPhoneVerified bool       `json:"is_phone_verified" db:"is_phone_verified"`
+	Role            string     `json:"role"`
+	CreatedAt       time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt       time.Time  `json:"updated_at" db:"updated_at"`
+	LastLogin       *time.Time `json:"las_login" db:"last_login"`
+}
